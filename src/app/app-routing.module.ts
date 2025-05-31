b4696a8efcf4befb5e822ee604f0c7cb8e3a398f
paola <maryps1608@gmail.com>
2025-05-30 22:32:10 -0400

    proyecto final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,20 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'info-personal',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'contacto',
+    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
   }
+
 ];
 @NgModule({
   imports: [
